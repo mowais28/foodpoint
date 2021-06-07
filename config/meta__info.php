47c3d044d2f963a4ee01fe24e20e6d22d9ce36a7
M Owais <59700860+mowais28@users.noreply.github.com>
2021-06-07 12:15:31 +0500

    Add files via upload

---
+++ config/meta__info.php
@@ -0,0 +1,7 @@
+<?php
+$_INFO = array(
+    'title' => 'Foodie', // website ConTitle 
+    'meta description' => 'Get Your Favorite Food in Few Clicks with Food Point.', // meta Desc
+    'meta tags' => 'online food order, restaurant, spicy food,food panda, food point', // Meta keywords
+    );
+?>
